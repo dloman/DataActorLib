fc8ad105280dd3c2fce618667ef60ef2059dd30e
Daniel Loman <dloman@toyon.com>
2017-07-15 12:45:11 -0700

    added check for wxapp

--- GuiStuff/Helpers.hpp
+++ GuiStuff/Helpers.hpp
@@ -2,12 +2,16 @@
 
 #include <wx/app.h>
 #include <wx/window.h>
+#include <iostream>
 namespace gs
 {
   template <typename T>
   void DoOnGuiThread(T&& function)
   {
-    wxTheApp->GetTopWindow()->GetEventHandler()->CallAfter(
-      std::forward<T>(function));
+    if (wxTheApp)
+    {
+      wxTheApp->GetTopWindow()->GetEventHandler()->CallAfter(
+        std::forward<T>(function));
+    }
   }
 }
